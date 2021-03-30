4428d697dc183120663dab74152b9bb07c4e7c68
ritvikpuri <ritvikpuri00@gmail.com>
2021-03-30 17:09:03 -0400

    first commit for website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
+import myImage from './img/me.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Haanji Kiven</h1>
+      <img src={myImage} alt="profile-image"></img>
     </div>
   );
 }
